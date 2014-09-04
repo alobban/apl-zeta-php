10f54b8d8ab221ba7752b2239d4c02dc3a792a75
Andrew Lobban <andrewglobban@gmail.com>
2014-09-04 13:29:25 -0500

    Add update to carousel

--- js/myjquery.js
+++ js/myjquery.js
@@ -11,6 +11,47 @@
 // var imageIndex = 0;
 
 window.onload = function() {
-	var bkgdImage = document.getElementById("bg-cover-rotator");
-	console.log(bkgdImage);
+	var bkgdParent = document.getElementById("bg-cover-rotator");
+	console.log(bkgdParent);
+	
+	var bkgdChildren = bkgdParent.getElementsByClassName("cover");
+	console.log(bkgdChildren.length);
+	
+	var cnt = 0, count = bkgdChildren.length;
+	console.log(cnt + " " + count);
+	
+	var input = "../apl-zeta/img/carousel/";
+	var imageArray = ["aplchi1.png","aplchi2.png","aplchi3.png","aplchi4.png","aplchi5.png"];
+	var imageIndex = 0;
+	
+	setInterval(carousel, 10000);
+	
+	function carousel() {
+		cnt = (cnt + 1) % count;
+		imageIndex = (imageIndex + 1) % imageArray.length;
+		console.log(cnt);
+		if (cnt === 0) {
+			bkgdChildren[cnt].style.background = "url(" + input + imageArray[imageIndex] + ") no-repeat center center fixed";
+			bkgdChildren[cnt].style.position = "relative";
+			bkgdChildren[cnt].style.height = "100%";
+			bkgdChildren[cnt].style.width = "100%";
+			bkgdChildren[cnt].style.backgroundSize = "cover";
+			bkgdChildren[cnt].style.webkitBackgroundSize = "cover";
+			
+			// switch the other background off
+			bkgdChildren[(cnt+1)%count].style.display = "none";
+			bkgdChildren[cnt].style.display = "block";
+		} else {
+			bkgdChildren[cnt].style.background = "url(" + input + imageArray[imageIndex] + ") no-repeat center center fixed";
+			bkgdChildren[cnt].style.position = "relative";
+			bkgdChildren[cnt].style.height = "100%";
+			bkgdChildren[cnt].style.width = "100%";
+			bkgdChildren[cnt].style.backgroundSize = "cover";
+			bkgdChildren[cnt].style.webkitBackgroundSize = "cover";
+			
+			// switch the other background off
+			bkgdChildren[(cnt+1)%count].style.display = "none";
+			bkgdChildren[cnt].style.display = "block";
+		}
+	} 
 };
