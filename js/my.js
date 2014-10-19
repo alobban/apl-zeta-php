0c8431139899ff8e70086774988fad3de79bedf9
Andrew Lobban <andrewglobban@gmail.com>
2014-10-19 13:48:04 -0500

    Adds initializer function

--- js/my.js
+++ js/my.js
@@ -11,8 +11,40 @@ $(document).ready(function() {
 	var imageIndex = 0;
 	var counter = 0;
 
+	function initializer() {
+		behindImage.style.background = "url(./img/jaguarHiRes.png) no-repeat center center scroll";
+		behindImage.style.display = "block";
+		behindImage.style.position = "absolute";
+		behindImage.style.height = "100%";
+		behindImage.style.width = "100%";
+		behindImage.style.paddingTop = "100px";
+		behindImage.style.backgroundSize = "cover";
+		behindImage.style.mozBackgroundSize = "cover";
+		behindImage.style.oBackgroundSize = "cover";
+		behindImage.style.webkitBackgroundSize = "cover";
+
+		$(frontImage).css("display", "none");
+		frontImage.style.background = "url(" + input + imageArray[imageIndex] + ") no-repeat center center scroll";
+		frontImage.style.position = "absolute";
+		frontImage.style.height = "100%";
+		frontImage.style.width = "100%";
+		frontImage.style.paddingTop = "100px";
+		frontImage.style.backgroundSize = "cover";
+		frontImage.style.mozBackgroundSize = "cover";
+		frontImage.style.oBackgroundSize = "cover";
+		frontImage.style.webkitBackgroundSize = "cover";
+		$(frontImage).fadeIn(2000);
+		console.log("First Run");
+	}
+
 	function carousel() {
-		changeImage();
+		if (counter < 1) {
+			initializer();
+			counter++;
+		} else {
+			changeImage();
+			// console.log(counter);
+		}
 	} 
 
 	function changeImage() {
